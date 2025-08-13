a47b8ba87834ee73037ddcbaf87a0d9d8d097ae2
Abinesh s <119126115+Abinesh-ss@users.noreply.github.com>
2025-08-13 12:52:47 +0530

    Delete static/JS/webcam.js

--- static/JS/webcam.js
+++
@@ -1,9 +0,0 @@
-const video = document.querySelector("#webcam");
-
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then((stream) => {
-    video.srcObject = stream;
-  })
-  .catch((err) => {
-    console.error("Webcam error: ", err);
-  });
